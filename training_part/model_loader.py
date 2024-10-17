fb7c8a68b2e5425de6c4a19ddb1c9b02dd9be5f3
Foundsheep <moondeokjong@gmail.com>
2024-10-17 09:06:34 +0900

    fix: every 500 epoch

--- training_part/model_loader.py
+++ training_part/model_loader.py
@@ -89,7 +89,7 @@ class SPRDiffusionModel(L.LightningModule):
 
         checkpoint_save_per_500 = ModelCheckpoint(
             save_top_k=-1,
-            every_n_epochs=3,
+            every_n_epochs=500,
             filename="{epoch}-{step}-{train_loss:.4f}_save_per_500"
         )
         
